2d913cf8b87e72136683e788b278cdb0ac398a7c
ast_vnovodid <v.novodid@astoundcommerce.com>
2024-04-13 16:15:24 +0300

    fix: name of the app

--- app/page.js
+++ app/page.js
@@ -5,10 +5,10 @@ const HomePage = () => {
         <div className='hero min-h-screen bg-base-200'>
             <div className='hero-content text-center'>
                 <div className='max-w-md'>
-                    <h1 className='text-6xl font-bold text-primary'>GPTGenius</h1>
+                    <h1 className='text-6xl font-bold text-primary'>Open AI Chat Tour</h1>
                     <p className='py-6 text-lg leading-loose'>
-                        GPTGenius: Your AI language companion. Powered by OpenAI, it
-                        enhances your conversations, content creation, and more!
+                    Open AI Chat Tour: Your AI language partner.
+                    Fueled by OpenAI, this tool boosts your discussions, content development, and beyond!
                     </p>
                     <Link href='/chat' className='btn btn-secondary '>
                         Get Started
